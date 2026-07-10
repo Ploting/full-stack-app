f67d8b34c737af78394e5eecc523dfabf0775c03
Ploting <105204874+Ploting@users.noreply.github.com>
2026-07-11 02:48:00 +0700

    Delete backend/dist directory

--- backend/dist/items/items.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { ItemsService } from './items.service';
-import type { Item } from './item.entity';
-import { CreateItemDto } from './create-item.dto';
-export declare class ItemsController {
-    private readonly itemsService;
-    constructor(itemsService: ItemsService);
-    findAll(): Promise<Item[]>;
-    create(createItemDto: CreateItemDto): Promise<Item>;
-}
--- backend/dist/items/items.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { Repository } from 'typeorm';
-import { Item } from './item.entity';
-import { CreateItemDto } from './create-item.dto';
-export declare class ItemsService {
-    private readonly itemsRepository;
-    constructor(itemsRepository: Repository<Item>);
-    findAll(): Promise<Item[]>;
-    create(createItemDto: CreateItemDto): Promise<Item>;
-}
